52757021c6f8302c9de149b5ff022a1897a14f18
ueli kunz <kunz@ideadapt.net>
2012-08-29 16:20:37 +0000

    tabs replace with 4 spaces

--- test/test-server.js
+++ test/test-server.js
@@ -6,69 +6,69 @@ if (typeof require !== "undefined") {
 buster.testCase("Functional tests with server:", {
 
     setUp: function () {
-		var http = require("http");
-		var fs = require("fs");
-		var rs = require("buster-resources");
-		var virtualDocRoot = "/styles";
-		var port = 9988;
-		var middleware = rs.resourceMiddleware.create(virtualDocRoot);
-		var sett = rs.resourceSet.create();
-		sett.addResource({ path: "/rules-remote-1.css", content: fs.readFileSync("fixtures/rules-remote-1.css").toString() });
-		sett.addResource({ path: "/rules-remote-2.css", content: fs.readFileSync("fixtures/rules-remote-2.css").toString() });
-		middleware.mount("/", sett);
+        var http = require("http");
+        var fs = require("fs");
+        var rs = require("buster-resources");
+        var virtualDocRoot = "/styles";
+        var port = 9988;
+        var middleware = rs.resourceMiddleware.create(virtualDocRoot);
+        var sett = rs.resourceSet.create();
+        sett.addResource({ path: "/rules-remote-1.css", content: fs.readFileSync("fixtures/rules-remote-1.css").toString() });
+        sett.addResource({ path: "/rules-remote-2.css", content: fs.readFileSync("fixtures/rules-remote-2.css").toString() });
+        middleware.mount("/", sett);
 
-		this.server = http.createServer(function (req, res) {
-			if (middleware.respond(req, res)) { return; }
-			res.writeHead(404);
-			res.end();
-		}).listen(port);
-		this.docRoot = "http://localhost:"+ port + virtualDocRoot + "/";
+        this.server = http.createServer(function (req, res) {
+            if (middleware.respond(req, res)) { return; }
+            res.writeHead(404);
+            res.end();
+        }).listen(port);
+        this.docRoot = "http://localhost:"+ port + virtualDocRoot + "/";
     },
     tearDown: function () {
-		this.server.close();
+        this.server.close();
     },
-	
-	"load css from url - multiple files": function(done) {
-		var markup = "<html><head></head><body></body></html>";
+    
+    "load css from url - multiple files": function(done) {
+        var markup = "<html><head></head><body></body></html>";
         var css = [ this.docRoot + "rules-remote-1.css" , this.docRoot + "rules-remote-2.css" ];
         var expected = {
-			total: 6+6
-		};
-		var oSize = function(obj) {
-				var size = 0, key;
-				for (key in obj) {
-					if (obj.hasOwnProperty(key)) size++;
-				}
-				return size;
-		};
+            total: 6+6
+        };
+        var oSize = function(obj) {
+                var size = 0, key;
+                for (key in obj) {
+                    if (obj.hasOwnProperty(key)) size++;
+                }
+                return size;
+        };
 
         lib.analyze(css, markup, null, null, function(result) {
-			var total = oSize(result.used);
-		
+            var total = oSize(result.used);
+        
             assert.equals(total, expected.total);
             done();
         });
-	},
-	
-	"load css from url - same files": function(done) {
-		var markup = "<html><head></head><body></body></html>";
+    },
+    
+    "load css from url - same files": function(done) {
+        var markup = "<html><head></head><body></body></html>";
         var css = [ this.docRoot + "rules-remote-1.css" , this.docRoot + "rules-remote-1.css" ];
         var expected = {
-			total: 6
-		};
-		var oSize = function(obj) {
-				var size = 0, key;
-				for (key in obj) {
-					if (obj.hasOwnProperty(key)) size++;
-				}
-				return size;
-		};
+            total: 6
+        };
+        var oSize = function(obj) {
+                var size = 0, key;
+                for (key in obj) {
+                    if (obj.hasOwnProperty(key)) size++;
+                }
+                return size;
+        };
 
         lib.analyze(css, markup, null, null, function(result) {
-			var total = oSize(result.used);
-		
+            var total = oSize(result.used);
+        
             assert.equals(total, expected.total);
             done();
         });
-	}	
+    }    
 });
