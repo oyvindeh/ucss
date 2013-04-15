a90d1dd32aa748c281e42d4fb04554097e6621a1
oyvindeh <oyvindeh@opera.com>
2013-04-15 11:02:20 +0200

    Refactor tests.

--- test/crawler.js
+++ test/crawler.js
@@ -9,40 +9,90 @@ if (typeof require !== "undefined") {
 }
 
 
+var pageSetOne = {
+    "/rules1.css": ".foo {} .bar {}",
+    "/rules2.css": ".foo {} .bar {} .baz {} .qux {}",
+    "/markup1.html":
+            ["<html>",
+            "  <head>",
+            "  </head>",
+            "  <body class='foo'>",
+            "    <a href='markup2.html'>markup2</a>",
+            "    <a href='markup1.html'>markup1</a>",
+            "  </body>",
+            "</html>"].join(""),
+    "/markup2.html":
+            ["<html>",
+            "  <head>",
+            "  </head>",
+            "  <body class='bar'>",
+            "    <a href='markup1.html'>markup1</a>",
+            "  </body>",
+            "</html>"].join(""),
+    "/external_links.html":
+            ["<html>",
+            "  <head>",
+            "  </head>",
+            "  <body class='bar'>",
+            "    <a href='http://127.0.0.1:9989/index.html'>index.html</a>",
+            "    <a href='http://127.0.0.1:9988/markup1.html'>markup1</a>",
+            "    <a href='/markup2.html'>markup1</a>",
+            "  </body>",
+            "</html>"].join(""),
+    "/path1/relative_paths.html":
+            ["<html>",
+            "  <head>",
+            "  </head>",
+            "  <body>",
+            "    <a href='relative1.html'>index.html</a>",
+            "    <a href='../relative2.html'>markup1</a>",
+            "    <a href='/relative3.html'>markup1</a>",
+            "    <a href='../path2/relative4.html'>markup1</a>",
+            "  </body>",
+            "</html>"].join(""),
+    "/path1/relative1.html":
+            ["<html>",
+            "  <head>",
+            "  </head>",
+            "  <body class='foo'>",
+            "  </body>",
+            "</html>"].join(""),
+    "/relative2.html":
+            ["<html>",
+            "  <head>",
+            "  </head>",
+            "  <body class='bar'>",
+            "  </body>",
+            "</html>"].join(""),
+    "/relative3.html":
+            ["<html>",
+            "  <head>",
+            "  </head>",
+            "  <body class='baz'>",
+            "  </body>",
+            "</html>"].join(""),
+    "/path2/relative4.html":
+            ["<html>",
+            "  <head>",
+            "  </head>",
+            "  <body class='qux'>",
+            "  </body>",
+            "</html>"].join("")
+};
+
+
+var pageSetTwo = {
+    "/index.html": "<html><head></head><body class='baz'></body></html>"
+};
+
+
 buster.testCase("uCSS crawler", {
     setUp: function () {
         var http = require("http");
 
         this.server = http.createServer(function (req, res) {
-            if ("/markup1.html" === req.url) {
-                res.end(["<html>",
-                         "  <head>",
-                         "  </head>",
-                         "  <body class='foo'>",
-                         "    <a href='markup2.html'>markup2</a>",
-                         "    <a href='markup1.html'>markup1</a>",
-                         "  </body>",
-                         "</html>"].join(""));
-            } else if ("/markup2.html" === req.url) {
-                res.end(["<html>",
-                         "  <head>",
-                         "  </head>",
-                         "  <body class='bar'>",
-                         "    <a href='markup1.html'>markup1</a>",
-                         "  </body>",
-                         "</html>"].join(""));
-            } else if ("/external_links.html" === req.url) {
-                res.end(["<html>",
-                         "  <head>",
-                         "  </head>",
-                         "  <body class='bar'>",
-                         "    <a href='http://127.0.0.1:9989/index.html'>index.html</a>",
-                         "    <a href='http://127.0.0.1:9988/markup1.html'>markup1</a>",
-                         "    <a href='/markup2.html'>markup1</a>",
-                         "  </body>",
-                         "</html>"].join(""));
-            } else if ("/rules1.css" === req.url) {
-                res.end(".foo {} .bar {}");
+            if (req.url in pageSetOne) {
+                res.end(pageSetOne[req.url]);
             } else {
                 res.writeHead(404);
                 res.end();
@@ -50,8 +100,11 @@ buster.testCase("uCSS crawler", {
         }).listen(9988, "0.0.0.0");
 
         this.anotherServer = http.createServer(function (req, res) {
-            if ("/index.html" === req.url) {
-                res.end("<html><head></head><body class='baz'></body></html>");
+            if (req.url in pageSetTwo) {
+                res.end(pageSetTwo[req.url]);
+            } else {
+                res.writeHead(404);
+                res.end();
             }
         }).listen(9989, "0.0.0.0");
     },
@@ -105,69 +158,11 @@ buster.testCase("uCSS crawler", {
             assert.equals(result.duplicates, expected.duplicates);
             done();
         });
-    }
-});
-
-buster.testCase("uCSS crawler also", {
-    setUp: function () {
-        var http = require("http");
-
-        this.server = http.createServer(function (req, res) {
-            if ("/path1/relative_paths.html" === req.url) {
-                res.end(["<html>",
-                         "  <head>",
-                         "  </head>",
-                         "  <body>",
-                         "    <a href='relative1.html'>index.html</a>",
-                         "    <a href='../relative2.html'>markup1</a>",
-                         "    <a href='/relative3.html'>markup1</a>",
-                         "    <a href='../path2/relative4.html'>markup1</a>",
-                         "  </body>",
-                         "</html>"].join(""));
-            } else if ("/path1/relative1.html" === req.url) {
-                res.end(["<html>",
-                         "  <head>",
-                         "  </head>",
-                         "  <body class='foo'>",
-                         "  </body>",
-                         "</html>"].join(""));
-            } else if ("/relative2.html" === req.url) {
-                res.end(["<html>",
-                         "  <head>",
-                         "  </head>",
-                         "  <body class='bar'>",
-                         "  </body>",
-                         "</html>"].join(""));
-            } else if ("/relative3.html" === req.url) {
-                res.end(["<html>",
-                         "  <head>",
-                         "  </head>",
-                         "  <body class='baz'>",
-                         "  </body>",
-                         "</html>"].join(""));
-            } else if ("/path2/relative4.html" === req.url) {
-                res.end(["<html>",
-                         "  <head>",
-                         "  </head>",
-                         "  <body class='qux'>",
-                         "  </body>",
-                         "</html>"].join(""));
-            } else if ("/rules1.css" === req.url) {
-                res.end(".foo {} .bar {} .baz {} .qux {}");
-            } else {
-                res.writeHead(404);
-                res.end();
-            }
-        }).listen(9988, "0.0.0.0");
-    },
-
-    tearDown: function () {
-        this.server.close();
     },
 
     "handles relative paths": function(done) {
         var markup = ["http://127.0.0.1:9988/path1/relative_paths.html"];
-        var css = ["http://127.0.0.1:9988/rules1.css"];
+        var css = ["http://127.0.0.1:9988/rules2.css"];
 
         var expected = {};
         expected.used = {};
