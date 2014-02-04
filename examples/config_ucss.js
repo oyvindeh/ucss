0fe587b863b81a065016c14ba40fa0727464d967
oyvindeh <oyvindeh@opera.com>
2014-02-04 12:10:21 +0100

    Add periods in comments.

--- examples/config_ucss.js
+++ examples/config_ucss.js
@@ -1,26 +1,26 @@
 module.exports = {
     "pages": { // (Required) Pages to check. Crawl or include is required.
-        "crawl": "http://localhost:8000/", // (Optional, if "include" is given)
+        "crawl": "http://localhost:8000/", // (Optional, if "include" is given).
                                            // Starting point for crawler.
-        "exclude": [ // (Optional) List of HTML files/URLs to check
+        "exclude": [ // (Optional) List of HTML files/URLs to check.
             "http://localhost:8000/admin/*",
             "http://localhost:8000/foo/"
         ],
-        "include": [ // (Optional, if "crawl" is given)
-                     // List of HTML files/URLs to check
+        "include": [ // (Optional, if "crawl" is given).
+                     // List of HTML files/URLs to check.
             "http://localhost:8001/unlinked_articles/1",
             "http://localhost:8001/unlinked_articles/2"
         ]
     },
-    "css": [ // (Required) List of CSS files to check
+    "css": [ // (Required) List of CSS files to check.
         "base.css"
     ],
     "whitelist": [".foo", ".bar"], // (Optional) List of CSS rules to ignore,
-                                   // e.g. ones added by JavaScript
-    "timeout": 4000, // (Optional) Timeout for HTTP requests
-                     // (default is 4000ms)
-    "auth": { // (Optional) Authentication information
-              // Please see docs for more info
+                                   // e.g. ones added by JavaScript.
+    "timeout": 4000, // (Optional) Timeout for HTTP requests.
+                     // (default is 4000ms).
+    "auth": { // (Optional) Authentication information.
+              // Please see docs for more info.
         "username": "foo",
         "password": "bar",
         "loginUrl": "http://localhost:8000/accounts/login/",
